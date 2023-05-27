e028604646c71b51bb7d406ce953ec700765dcfb
ghody <ghody@localhost.localdomain>
2023-05-27 20:16:08 +0200

    created tables: doctor,user,provider,appoitment and medical records

---
+++ src/api/appoitment/routes/appoitment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appoitment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::appoitment.appoitment');
---
+++ src/api/appoitment/services/appoitment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appoitment service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::appoitment.appoitment');
